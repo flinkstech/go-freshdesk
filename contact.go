43b25cd526a0a5d9c041d1a6bd587dcda39bc631
Etienne Asselin <easselin@flinks.io>
2019-04-10 16:04:19 -0400

    change time.Time in structs to pointers for omitempty tag

--- contact.go
+++ contact.go
@@ -3,15 +3,15 @@ package freshdesk
 import "time"
 
 type Contact struct {
-	Name        string    `json:"name,omitempty"`
-	Active      string    `json:"active,omitempty"`
-	Email       string    `json:"email,omitempty"`
-	JobTitle    string    `json:"job_title,omitempty"`
-	Language    string    `json:"language,omitempty"`
-	LastLoginAt time.Time `json:"last_login_at,omitempty"`
-	Mobile      int       `json:"mobile,omitempty"`
-	Phone       int       `json:"phone,omitempty"`
-	TimeZone    string    `json:"time_zone,omitempty"`
-	CreatedAt   time.Time `json:"created_at,omitempty"`
-	UpdatedAt   time.Time `json:"updated_at,omitempty"`
+	Name        string     `json:"name,omitempty"`
+	Active      string     `json:"active,omitempty"`
+	Email       string     `json:"email,omitempty"`
+	JobTitle    string     `json:"job_title,omitempty"`
+	Language    string     `json:"language,omitempty"`
+	LastLoginAt *time.Time `json:"last_login_at,omitempty"`
+	Mobile      int        `json:"mobile,omitempty"`
+	Phone       int        `json:"phone,omitempty"`
+	TimeZone    string     `json:"time_zone,omitempty"`
+	CreatedAt   *time.Time `json:"created_at,omitempty"`
+	UpdatedAt   *time.Time `json:"updated_at,omitempty"`
 }
